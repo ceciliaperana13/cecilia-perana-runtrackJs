56d407c38f1c35f2a4b1d39376ec9059f5a79cb6
ceciliaperana13 <cperana13@gmail.com>
2026-04-23 12:11:17 +0200

    job2 fini

--- jour4/job2/scritp.js
+++ jour4/job2/scritp.js
@@ -1,1 +1,21 @@
-//Créez une fonction JavaScript “jsonValueKey()” qui prend en paramètre une chaîne de caractères au format JSON et une clé.
+function jsonValueKey(jsonString, key) {
+    try {
+        const obj = JSON.parse(jsonString);
+        return obj[key];
+    } catch (error) {
+        console.error("JSON invalide");
+        return null;
+    }
+}
+
+// json ici
+const json = `{
+    "name": "Laplateforme_",
+    "address": "8 rue d'hozier",
+    "city": "Marseille",
+    "nb_staff": "11",
+    "creation": "2019"
+}`;
+
+// Test
+console.log(jsonValueKey(json, "city"));
